eb3efc76bc29ca3d736adf73c44ac02d2ca3ca94
Derek Shigetomi <derekshigetomi.dgs@gmail.com>
2019-12-01 19:46:22 -0500

    fixed animation

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-// $(document).ready(function() {
-
-	// $(window).on("load", function(){
-	//     $('h1').addClass('animated fadeInDown');
-	// });
-
-// });
-
 $(document).ready(function() {
 
-	$(window).scroll( function(){
-
-	    /* Check the location of each desired element */
-	    $('.text-muted').each( function(i){
-
-	        var bottom_of_object = $(this).position().top + $(this).outerHeight();
-	        var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-	        /* If the object is completely visible in the window, fade it it */
-	        if( bottom_of_window > bottom_of_object ){
-
-	            $(".mt-5").addClass('animated fadeInLeft delay-0.8s');
-
-	        }    
-	    }); 
-	}); 
+	$(window).on("load", function(){
+	    $('h1').addClass('animated fadeInDown');
+	});
 
 });
+
