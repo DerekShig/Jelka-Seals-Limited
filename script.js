993338af5c60ef9d10bf2031a649a066fa5572ca
Derek Shigetomi <derekshigetomi.dgs@gmail.com>
2019-12-01 19:09:10 -0500

    added animations for service section

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+// $(document).ready(function() {
+
+	// $(window).on("load", function(){
+	//     $('h1').addClass('animated fadeInDown');
+	// });
+
+// });
+
 $(document).ready(function() {
 
-	$(window).on("load", function(){
-	    $('h1').addClass('animated fadeInDown');
-	});
-	
+	$(window).scroll( function(){
+
+	    /* Check the location of each desired element */
+	    $('.text-muted').each( function(i){
+
+	        var bottom_of_object = $(this).position().top + $(this).outerHeight();
+	        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+	        /* If the object is completely visible in the window, fade it it */
+	        if( bottom_of_window > bottom_of_object ){
+
+	            $(".mt-5").addClass('animated fadeInLeft delay-0.8s');
+
+	        }    
+	    }); 
+	}); 
+
 });
