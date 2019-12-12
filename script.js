c39f388f0f75b07981615c4833dc0d450abbd77d
Derek Shigetomi <derekshigetomi.dgs@gmail.com>
2019-12-11 23:20:43 -0500

    fixed typos / added some content on products page

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ $(document).ready(function() {
 	    $('.cover-page').addClass('animated fadeInDown');
 	});
 
+  $('.btn-modal').on('click', function() {
+      $("#showImg").empty();
+      var image = $(this).attr("name");
+      $("#showImg").append(`<img class='img-fluid' src="${image}"/>`)
+
+      $(".modal-title").empty();
+      var txt = $(this).attr("id");
+      $(".modal-title").append(`<p>${txt}</p>`);
+  })
+
 });
