bb8b393c7625abe668e15bed2e9bcf0ea79cedc6
Derek Shigetomi <derekshigetomi.dgs@gmail.com>
2020-01-21 21:29:10 -0500

    added products

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ $(document).ready(function() {
 	    $('.cover-page').addClass('animated fadeInDown');
 	});
 
-  $('.btn-modal').on('click', function() {
-      $("#showImg").empty();
-      var image = $(this).attr("name");
-      $("#showImg").append(`<img class='img-fluid' src="${image}"/>`)
+  // $('.btn-modal').on('click', function() {
+  //     $("#showImg").empty();
+  //     var image = $(this).attr("name");
+  //     $("#showImg").append(`<img class='img-fluid' src="${image}"/>`)
 
-      $(".modal-title").empty();
-      var txt = $(this).attr("id");
-      $(".modal-title").append(`<p>${txt}</p>`);
-  });
+  //     $(".modal-title").empty();
+  //     var txt = $(this).attr("id");
+  //     $(".modal-title").append(`<p>${txt}</p>`);
+  // });
+
+  $('td img').attr('data-toggle', 'modal');
+  $('td img').attr('data-target', '#myModal');
 
   $('td img').on('click', function() {
 
