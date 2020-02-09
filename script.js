1f27f19188145cd428da1294faefc38bab34a7f0
Derek Shigetomi <derekshigetomi.dgs@gmail.com>
2020-02-08 19:40:43 -0500

    changed links

--- script.js
+++ script.js
@@ -4,16 +4,6 @@ $(document).ready(function() {
 	    $('.cover-page').addClass('animated fadeInDown');
 	});
 
-  // $('.btn-modal').on('click', function() {
-  //     $("#showImg").empty();
-  //     var image = $(this).attr("name");
-  //     $("#showImg").append(`<img class='img-fluid' src="${image}"/>`)
-
-  //     $(".modal-title").empty();
-  //     var txt = $(this).attr("id");
-  //     $(".modal-title").append(`<p>${txt}</p>`);
-  // });
-
   $('td img').attr('data-toggle', 'modal');
   $('td img').attr('data-target', '#myModal');
 
